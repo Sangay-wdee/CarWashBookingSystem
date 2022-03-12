8d8db617fc73b41d3a01cc6a7fb5728175f482d9
Sangay-wdee <02180422.cst@rub.edu.bt>
2022-03-12 12:41:56 +0530

    third commit

---
+++ serviceSP.js
@@ -0,0 +1,172 @@
+//hovering active
+var header = document.getElementById("navbar");
+var link = header.getElementsByClassName("menu");
+for (var i = 0; i < link.length; i++) {
+  link[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+// login&signup
+function SignIn(){
+  let user = document.getElementById("signin");
+  let change = location.replace("loginPage.html");
+  user.appendChild(change)
+}
+function SignUp(){
+  let user = document.getElementById("signin");
+  let change = location.replace("registerUser.html");
+  user.appendChild(change)
+}
+
+
+//header scroll
+(function(){
+
+    var doc = document.documentElement;
+    var w   = window;
+
+    /*
+    define four variables: curScroll, prevScroll, curDirection, prevDirection
+    */
+
+    var curScroll;
+    var prevScroll = w.scrollY || doc.scrollTop;
+    var curDirection = 0;
+    var prevDirection = 0;
+
+    /*
+    how it works:
+    -------------
+    create a scroll event listener
+    create function to check scroll position on each scroll event,
+    compare curScroll and prevScroll values to find the scroll direction
+    scroll up - 1, scroll down - 2, initial - 0
+    then set the direction value to curDirection
+    compare curDirection and prevDirection
+    if it is different, call a function to show or hide the header
+    example:
+    step 1: user scrolls down: curDirection 2, prevDirection 0 > hide header
+    step 2: user scrolls down again: curDirection 2, prevDirection 2 > already hidden, do nothing
+    step 3: user scrolls up: curDirection 1, prevDirection 2 > show header
+    */
+
+    var header = document.getElementById('site-header');
+    var toggled;
+    var threshold = 300;
+
+    var checkScroll = function() {
+        curScroll = w.scrollY || doc.scrollTop;
+        if(curScroll > prevScroll) {
+            // scrolled down
+            curDirection = 2;
+        }
+        else {
+            //scrolled up
+            curDirection = 1;
+        }
+
+        if(curDirection !== prevDirection) {
+            toggled = toggleHeader();
+        }
+
+        prevScroll = curScroll;
+        if(toggled) {
+            prevDirection = curDirection;
+        }
+    };
+
+    var toggleHeader = function() { 
+        toggled = true;
+        if(curDirection === 2 && curScroll > threshold) {
+            header.classList.add('hide');
+        }
+        else if (curDirection === 1) {
+            header.classList.remove('hide');
+        }
+        else {
+            toggled = false;
+        }
+        return toggled;
+    };
+
+    window.addEventListener('scroll', checkScroll);
+
+})();
+
+//hamburger dropdown
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('#hamburg')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  
+ 
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+ 
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
